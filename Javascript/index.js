b292aae391f9597811c7d08f4c6a770328748dc4
Bruno Barbagelata <bruno140685@gmail.com>
2021-08-30 15:16:13 -0400

    added assets

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+function drawRiver() {
+  const river = new Image();
+  river.src = "../Assets/river.jpg";
+  river.onload = function () {
+    ctx.drawImage(road, 0, 0, 600, 600);
+  };
+}
+
+drawRiver();
